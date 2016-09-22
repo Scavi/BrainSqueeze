39c7a83c2c8c515932c164b6c18cc74b12a6ff79
Michael Heymel <cfa.scavenger@gmx.de>
2016-09-22 18:35:23 +0200

    new answer to the question to find the minimum subarray + comment

--- src/com/scavi/brainsqueeze/puzzle/ArrayPuzzle.java
+++ src/com/scavi/brainsqueeze/puzzle/ArrayPuzzle.java
@@ -1,11 +1,59 @@
 package com.scavi.brainsqueeze.puzzle;
 
 /**
- * Created by Scavenger on 15/09/16.
+ * Created by Michael Heymel on 15/09/16.
  */
 public class ArrayPuzzle {
 
 
+    /**
+     * Find the minimum of the sub-array of the given subarray size.
+     *
+     * @param input        the input array
+     * @param subarraySize the size of the subarray
+     * @return the minimum size
+     */
+    public int findMinimumOfSubarray(final int[] input, final int subarraySize) {
+
+        if (input == null) {
+            throw new IllegalArgumentException("Illegal input array: <null>");
+        }
+        if (input.length < subarraySize) {
+            throw new IllegalArgumentException("Subarray size bigger then input array!");
+        }
+
+        int min = 0;
+        for (int i = 0; i < subarraySize; ++i) {
+            min += input[i];
+        }
+
+        int tmp = min;
+        for (int i = subarraySize; i < input.length; ++i) {
+            tmp -= input[i - subarraySize];
+            tmp += input[i];
+            min = Math.min(min, tmp);
+        }
+        return min;
+    }
+
+
+    /**
+     * You have an unordered array X of n integers. Find the array M containing n elements where Mi
+     * is the product of all integers in X except for Xi. You may not use division. You can use
+     * extra memory. (Hint: There are solutions faster than O(n2).)
+     * <p/>
+     * For example with the integer array: 5 3 2 6 4
+     * <p/>
+     * 144 (3 * 2 * 6 * 4)
+     * 240 (5 * 2 * 6 * 4)
+     * 360 (5 * 3 * 6 * 4)
+     * 120 (5 * 3 * 2 * 4)
+     * 180 (5 * 3 * 2 * 6)
+     * The algorithm runs with O(n) time and O(n) space.
+     *
+     * @param input the input array with the values
+     * @return the input array with the values
+     */
     public int[] solve(final int[] input) {
         if (input == null || input.length < 2) {
             throw new IllegalArgumentException("Input array needs at least the size of 2");
@@ -22,6 +70,7 @@ public class ArrayPuzzle {
         }
         return output;
     }
+
 
     private void prepareCaches(int[] input, int[] leftToRight, int[] rightToLeft) {
         int inputLength = input.length;
