29bc30ef46cfea042c882c63839650f31ad4c1dd
Michael Heymel <cfa.scavenger@gmx.de>
2016-09-22 18:32:49 +0200

    uni tests for the water glass tower question

---
+++ test/com/scavi/brainsqueeze/dp/WaterGlassTowerTest.java
@@ -0,0 +1,47 @@
+package com.scavi.brainsqueeze.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Michael Heymel on 21/09/16.
+ */
+public class WaterGlassTowerTest {
+    @Test
+    public void test1() {
+        WaterGlassTower glassTower = new WaterGlassTower();
+        float result = glassTower.findWaterAmount(2.0f, 2, 2);
+        Assert.assertEquals(0.5f, result, 0.0f);
+    }
+
+
+    @Test
+    public void test2() {
+        WaterGlassTower glassTower = new WaterGlassTower();
+        float result = glassTower.findWaterAmount(10.0f, 4, 4);
+        Assert.assertEquals(0.375f, result, 0.0f);
+    }
+
+
+    @Test
+    public void test3() {
+        WaterGlassTower glassTower = new WaterGlassTower();
+        float result = glassTower.findWaterAmount(10.0f, 3, 4);
+        Assert.assertEquals(1.0f, result, 0.0f);
+    }
+
+
+    @Test
+    public void test4() {
+        WaterGlassTower glassTower = new WaterGlassTower();
+        float result = glassTower.findWaterAmount(20.0f, 3, 7);
+        Assert.assertEquals(0.71875, result, 0.0f);
+    }
+
+    @Test
+    public void test5() {
+        WaterGlassTower glassTower = new WaterGlassTower();
+        float result = glassTower.findWaterAmount(42.0f, 6, 6);
+        Assert.assertEquals(0.34375f, result, 0.0f);
+    }
+}
