64ac2c16876af68a37a999bc03f8a73f5891a0d1
Michael Heymel <cfa.scavenger@gmx.de>
2017-02-26 20:09:07 +0100

    Array question to find the sub-array with the least average

---
+++ src/main/java/com/scavi/brainsqueeze/general/ArrayGames.java
@@ -0,0 +1,157 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.scavi.brainsqueeze.general;
+
+import com.google.common.base.Preconditions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Michael Heymel
+ * @since 05/02/17
+ */
+public class ArrayGames {
+
+    /**
+     * Find the sub-array with least average. Given an array arr[] of size n and integer k such that
+     * k <= n.
+     * <p>
+     * Input:  arr[] = {3, 7, 90, 20, 10, 50, 40}, k = 3 Output: Subarray between indexes 3 and 5
+     * The subarray {20, 10, 50} has the least average among all subarrays of size 3.
+     * <p>
+     * Input:  arr[] = {3, 7, 5, 20, -10, 0, 12}, k = 2 Output: Subarray between [4, 5] has minimum
+     * average.
+     * <p>
+     * The algorithm takes O(n) time and O(1) space
+     *
+     * @param inputData   the input data
+     * @param averageSize the average size
+     * @return the positions within the subarray
+     */
+    public int[] leastAverage(final int[] inputData, final int averageSize) {
+        Preconditions.checkNotNull(inputData, "Illegal input data: <null>");
+        Preconditions.checkArgument(inputData.length >= averageSize,
+                "Input data contains less elements than average size");
+
+        int sum = 0;
+        for (int i = 0; i < averageSize; ++i) {
+            sum += inputData[i];
+        }
+        int[] result = new int[2];
+        result[0] = 0;
+        result[1] = averageSize - 1;
+        int tmpSum = sum;
+        for (int currentEnd = averageSize; currentEnd < inputData.length; ++currentEnd) {
+            tmpSum -= inputData[currentEnd - averageSize];
+            tmpSum += inputData[currentEnd];
+            if (tmpSum < sum) {
+                result[0] = currentEnd - averageSize + 1;
+                result[1] = currentEnd;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * Given an integer array, determine the longest sequence of adjacent value that will result in
+     * a sum of 0. E.g.  1, 2, 3, 4, -9, 6, 7, -8, 1, 9 will result in 5 (from 4 to -8)
+     * <p>
+     * This method determines the longest sequence of integers that can get sum up to result in the
+     * value of 0. This method runs in O(n) and uses O(m) space.
+     *
+     * @param inputData the input data
+     * @return the longest sequence or <code>0</code>
+     */
+    public int determineLongestZeroSum(final int[] inputData) {
+        Preconditions.checkNotNull(inputData, "Illegal input data: <null>");
+        int longestSumZero = 0;
+        if (inputData.length > 0) {
+            final Map<Integer, Integer> cacheData = new HashMap<>();
+            int currentSum = 0;
+            for (int i = 0; i < inputData.length; ++i) {
+
+                currentSum += inputData[i];
+                if (currentSum == 0) {
+                    longestSumZero = Math.max(longestSumZero, i);
+                } else if (cacheData.get(currentSum) != null) {
+                    longestSumZero = Math.max(longestSumZero, i - cacheData.get(currentSum));
+                } else {
+                    cacheData.put(currentSum, i);
+                }
+            }
+        }
+        return longestSumZero;
+    }
+
+
+    /**
+     * Verify if a given lookup value is in a sorted matrix (where width and height are equal) in
+     * better than O(n).
+     *
+     * @param matrix      the sorted matrix
+     * @param lookupValue the lookup value for the matrix
+     * @return <code>true</code> the value exists <p/> <code>false</code> the value doesn't exist
+     */
+    public boolean isValueExisting(final int[][] matrix, final int lookupValue) {
+        Preconditions.checkNotNull(matrix, "Illegal input data: <null>");
+        int height = matrix.length;
+        int width = height > 0 ? matrix[0].length : 0;
+        if (height == 0 || width == 0) {
+            return false;
+        }
+        boolean isExisting = false;
+        int left = 0;
+        int right = height * width;
+
+        while (!isExisting) {
+            int mid = (right + left) / 2;
+            int currentX = mid % height;
+            int currentY = mid / width;
+
+            if (matrix[currentY][currentX] == lookupValue) {
+                isExisting = true;
+            } else {
+                if (matrix[currentY][currentX] > lookupValue) {
+                    if (right == mid) {
+                        return false;
+                    }
+                    right = mid;
+                } else {
+                    if (left == mid) {
+                        return false;
+                    }
+                    left = mid;
+                }
+            }
+        }
+        return isExisting;
+    }
+}
---
+++ src/test/java/com/scavi/brainsqueeze/general/ArrayGamesTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.scavi.brainsqueeze.general;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Michael Heymel
+ * @since 05/02/17
+ */
+public class ArrayGamesTest {
+
+    @Test
+    public void testLeastAverage1() {
+        ArrayGames arrayGames = new ArrayGames();
+        int[] result = arrayGames.leastAverage(new int[]{3, 7, 90, 20, 10, 50, 40}, 3);
+        Assert.assertEquals(3, result[0]);
+        Assert.assertEquals(5, result[1]);
+    }
+
+
+    @Test
+    public void testLeastAverage2() {
+        ArrayGames arrayGames = new ArrayGames();
+        int[] result = arrayGames.leastAverage(new int[]{3, 7, 5, 20, -10, 0, 12}, 2);
+        Assert.assertEquals(4, result[0]);
+        Assert.assertEquals(5, result[1]);
+    }
+
+
+    @Test
+    public void testLongestZeroSum1() {
+        ArrayGames arrayGames = new ArrayGames();
+        int result = arrayGames.determineLongestZeroSum(new int[]{1, 2, 3, 4, -9, 6, 7, -8, 1, 9});
+        Assert.assertEquals(5, result);
+    }
+
+
+    @Test
+    public void test2DimArrayLookup1() {
+        int[][] input = new int[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 15, 16},
+                                    {17, 18, 19, 20, 21}, {22, 23, 24, 25, 26}};
+        ArrayGames arrayGames = new ArrayGames();
+        for (int i = 1; i <= 26; ++i) {
+            boolean isExisting = arrayGames.isValueExisting(input, i);
+
+            if (i == 14) {
+                Assert.assertFalse(isExisting);
+            } else {
+                Assert.assertTrue(isExisting);
+            }
+        }
+    }
+}
