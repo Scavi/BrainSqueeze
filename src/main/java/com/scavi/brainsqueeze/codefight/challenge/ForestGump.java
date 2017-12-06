9ffede1f4609914a62ddb9b3a5d1a22f35ad78f7
Michael Heymel <cfa.scavenger@gmx.de>
2017-12-06 17:57:59 +0100

    shorter version of the puzzle

--- src/main/java/com/scavi/brainsqueeze/codefight/challenge/ForestGump.java
+++ src/main/java/com/scavi/brainsqueeze/codefight/challenge/ForestGump.java
@@ -1,17 +1,11 @@
 package com.scavi.brainsqueeze.codefight.challenge;
 
 public class ForestGump {
-    int i,j,p;
-    int runForerstRun(int[] d, int[] s) {
-        for (; i < d.length; ++i) {
-            int e = 0;
-            for (j = i; j < d.length && e >= 0; ++j) {
-                p = j % d.length;
-                e += s[p] - d[p];
-            }
-            if (e >= 0)
-                return i + 1;
-        }
-        return -1;
+    int i, m = Integer.MAX_VALUE, r = m;
+
+    int runForerstRun(int[] distance, int[] shrimp) {
+        for (int d : distance)
+            r = shrimp[i++] - d >= 0 ? Math.min(r, i) : m;
+        return r;
     }
 }
