f50d1d769d90c5102cb1e8bd1e6abde6e9f499d6
Michael Heymel <cfa.scavenger@gmx.de>
2017-12-06 17:30:38 +0100

    solution for codefight

--- src/test/java/com/scavi/brainsqueeze/codefight/challenge/ForestGumpTest.java
+++ src/test/java/com/scavi/brainsqueeze/codefight/challenge/ForestGumpTest.java
@@ -1,13 +1,19 @@
 package com.scavi.brainsqueeze.codefight.challenge;
 
 import org.junit.Test;
+
 import static com.google.common.truth.Truth.*;
 
 public class ForestGumpTest {
     @Test
     public void test1() {
-        int pos = new ForestGump().runForerstRun(new int[]{1,2,3}, new int[]{1,2,3});
+        int pos = new ForestGump().runForerstRun(new int[]{1, 2, 3}, new int[]{1, 2, 3});
         assertThat(pos).isEqualTo(1);
     }
 
+    @Test
+    public void test2() {
+        int pos = new ForestGump().runForerstRun(new int[]{1, 1, 1}, new int[]{0, 0, 3});
+        assertThat(pos).isEqualTo(3);
+    }
 }
