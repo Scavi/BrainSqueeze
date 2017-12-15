77db7a269ee758b1cb6bda36fe48f82878d8c7ab
Michael Heymel <cfa.scavenger@gmx.de>
2017-12-15 13:56:38 +0100

    solved day 15

---
+++ src/test/java/com/scavi/brainsqueeze/adventofcode/y2017/Day15DuelingGeneratorsTest.java
@@ -0,0 +1,33 @@
+package com.scavi.brainsqueeze.adventofcode.y2017;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.*;
+
+public class Day15DuelingGeneratorsTest {
+    @Test
+    public void test1() {
+        int result = new Day15DuelingGenerators().solveA(65, 8921);
+        assertThat(result).isEqualTo(588);
+    }
+
+    @Test
+    public void testQuestion1() {
+        int result = new Day15DuelingGenerators().solveA(512, 191);
+        assertThat(result).isEqualTo(567);
+    }
+
+
+    @Test
+    public void test2() {
+        int result = new Day15DuelingGenerators().solveB(65, 8921);
+        assertThat(result).isEqualTo(309);
+    }
+
+
+    @Test
+    public void testQuestion2() {
+        int result = new Day15DuelingGenerators().solveB(512, 191);
+        assertThat(result).isEqualTo(323);
+    }
+}
