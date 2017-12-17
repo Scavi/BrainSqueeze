8d87b90ea1c926104076c4dd019953727b0e3469
Michael Heymel <cfa.scavenger@gmx.de>
2017-12-17 10:51:36 +0100

    removed unnecessary code

--- src/main/java/com/scavi/brainsqueeze/adventofcode/y2017/Day13PacketScanners.java
+++ src/main/java/com/scavi/brainsqueeze/adventofcode/y2017/Day13PacketScanners.java
@@ -1,51 +1,24 @@
 package com.scavi.brainsqueeze.adventofcode.y2017;
 
 import com.scavi.brainsqueeze.adventofcode.util.AoCInputParser;
-import com.scavi.brainsqueeze.util.MathHelper;
-
-import java.math.BigInteger;
 
 public class Day13PacketScanners {
     private static final int DEPTH = 0;
     private static final int RANGE = 1;
 
     public int solveA(final String[] input) {
-        return solve(input, 0, false);
-    }
-
-    public int solveBruteForceB(final String[] input) {
-        int delay = 0;
-        while (solve(input, delay, true) != 0) {
-            delay++;
-        }
-        return delay;
-    }
-
-    public int solveB(final String[] input) {
-        int max = 15; //5000000;
-        // TODO
-        return 0;
-    }
-
-
-    private int solve(final String[] input, final int offset, final boolean isSneaking) {
         int[][] scanner = AoCInputParser.arrayPairFrom(input);
         int scannerRange = scanner[scanner.length - 1][DEPTH];
         int severity = 0;
-
-        int tmp, lookup;
+        int lookup;
         int scannerPos = 0;
         for (int i = 0; i <= scannerRange; ++i) {
             int depth = scanner[scannerPos][DEPTH];
             if (i == depth) {
-                tmp = i + offset;
-                lookup = (scanner[scannerPos][RANGE] - 1) * 2;
                 // due to the top down movement, the scanner reaches the top position at (depth - 1) * 2,
                 // e.g. for depth 3, it is 0, 4, 8, 12, 16...
-                if (MathHelper.gcd(tmp, lookup) == lookup) {
-                    if (isSneaking) {
-                        return -1;
-                    }
+                lookup = (scanner[scannerPos][RANGE] - 1) * 2;
+                if (i % lookup == 0) {
                     severity += (i * scanner[scannerPos][RANGE]);
                 }
                 scannerPos++;
@@ -53,4 +26,67 @@ public class Day13PacketScanners {
         }
         return severity;
     }
+
+    public int solveBruteForceB(final String[] input) {
+        int delay = 0;
+//        while (solve(input, delay, true) != 0) {
+//            delay++;
+//        }
+        return delay;
+    }
+
+    public int solveB(final String[] input) {
+        int max = 15; //5000000;
+        boolean[] toWait = new boolean[max];
+
+        int[][] scanner = AoCInputParser.arrayPairFrom(input);
+        int scannerRange = scanner[scanner.length - 1][DEPTH];
+
+        int scannerPos = 0;
+        for (int i = 0; i <= scannerRange; ++i) {
+            int depth = scanner[scannerPos][DEPTH];
+            if (i == depth) {
+
+                int lookup = (scanner[scannerPos][RANGE] - 1) * 2;
+                while (lookup < max) {
+                    toWait[lookup] = true;
+                    lookup += (scanner[scannerPos][RANGE] - 1) * 2;
+                }
+                scannerPos++;
+            }
+        }
+        for (int i = 0; i < max; ++i) {
+            if (!toWait[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+//    private int solve(final String[] input, final int offset, final boolean isSneaking) {
+//        int[][] scanner = AoCInputParser.arrayPairFrom(input);
+//        int scannerRange = scanner[scanner.length - 1][DEPTH];
+//        int severity = 0;
+//
+//        int tmp, lookup;
+//        int scannerPos = 0;
+//        for (int i = 0; i <= scannerRange; ++i) {
+//            int depth = scanner[scannerPos][DEPTH];
+//            if (i == depth) {
+//                tmp = i + offset;
+//                lookup = (scanner[scannerPos][RANGE] - 1) * 2;
+//                // due to the top down movement, the scanner reaches the top position at (depth - 1) * 2,
+//                // e.g. for depth 3, it is 0, 4, 8, 12, 16...
+//                if (IntMath.gcd(tmp, lookup) == lookup) {
+//                    if (isSneaking) {
+//                        return -1;
+//                    }
+//                    severity += (i * scanner[scannerPos][RANGE]);
+//                }
+//                scannerPos++;
+//            }
+//        }
+//        return severity;
+//    }
 }
