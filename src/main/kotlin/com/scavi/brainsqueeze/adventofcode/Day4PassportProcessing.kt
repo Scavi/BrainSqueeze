f7af8ea635e5e5aea9fdb2fb6678c6f6766babe1
heymel <heymel@amazon.de>
2020-12-05 00:06:36 +0100

    Reduced lines for AOC 2020, Day4 (https://github.com/shaeberling/euler/blob/master/kotlin/src/com/s13g/aoc/aoc2020/Day4.kt)
    Reduced the original solution by 12 lines after comparing and discussing this solution.

--- src/main/kotlin/com/scavi/brainsqueeze/adventofcode/Day4PassportProcessing.kt
+++ src/main/kotlin/com/scavi/brainsqueeze/adventofcode/Day4PassportProcessing.kt
@@ -1,9 +1,9 @@
 package com.scavi.brainsqueeze.adventofcode
 
 class Day4PassportProcessing {
-    private val mandatoryFields = mapOf("byr" to ::vByr, "iyr" to ::vIyr, "eyr" to ::vEyr,
-            "hgt" to ::vHgt, "hcl" to ::vHcl, "ecl" to ::vEcl, "pid" to ::vPid)
+    private val mandatoryFields = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
     private val pidRegex = """^\d{9}$""".toRegex()
+    private val hgtRegex = """^(\d+)(cm|in)$""".toRegex()
     private val hclRegex = """#[a-f0-9]{6}""".toRegex()
     private val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
@@ -16,11 +16,11 @@ class Day4PassportProcessing {
             }
             if (details.size >= 7) {
                 var isValid = true
-                for ((k, v) in mandatoryFields) {
-                    isValid = isValid && details.contains(k)
-                    isValid = if (isStrict) isValid && v(details.getOrDefault(k, "")) else isValid
-                    if (!isValid) {
-                        break
+                for (field in mandatoryFields) {
+                    isValid = isValid && details.contains(field)
+                    if (isStrict) {
+                        val value = details.getOrDefault(field, "")
+                        isValid = isValid && value.isNotEmpty() && validate(field, value)
                     }
                 }
                 validPassports += if (isValid) 1 else 0
@@ -29,39 +29,27 @@ class Day4PassportProcessing {
         return validPassports
     }
 
-    private fun vByr(value: String): Boolean {
-        return value.toInt() in 1920..2002
-    }
-
-    private fun vIyr(value: String): Boolean {
-        return value.toInt() in 2010..2020
-    }
-
-    private fun vEyr(value: String): Boolean {
-        return value.toInt() in 2020..2030
-    }
-
-    private fun vHgt(value: String): Boolean {
-        if (value.length > 2) {
-            val number = value.subSequence(0, value.length - 2).toString().toInt()
-            if (value.endsWith("cm")) {
-                return number in 150..193
-            } else if (value.endsWith("in")) {
-                return number in 59..76
+    private fun validate(field: String, value: String) : Boolean {
+        return when (field) {
+            "byr" -> value.toInt() in 1920..2002
+            "iyr" -> value.toInt() in 2010..2020
+            "eyr" -> value.toInt() in 2020..2030
+            "hgt" -> {
+                val groups = hgtRegex.find(value)
+                if (groups == null) false
+                else {
+                    val number = groups.groupValues[1].toInt()
+                    when(groups.groupValues[2]) {
+                        "cm" -> number in 150..193
+                        "in" -> number in 59..76
+                        else -> false
+                    }
+                }
             }
+            "hcl" -> hclRegex.containsMatchIn(value)
+            "ecl" -> eyeColors.contains(value)
+            "pid" -> pidRegex.containsMatchIn(value)
+            else -> false
         }
-        return false
-    }
-
-    private fun vHcl(value: String): Boolean {
-        return hclRegex.containsMatchIn(value)
-    }
-
-    private fun vEcl(value: String): Boolean {
-        return eyeColors.contains(value)
-    }
-
-    private fun vPid(value: String): Boolean {
-        return pidRegex.containsMatchIn(value)
     }
 }
