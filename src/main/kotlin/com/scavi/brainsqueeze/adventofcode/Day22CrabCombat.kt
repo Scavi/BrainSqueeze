79550cb8ce504295f762e9439e806b1acfa057c9
heymel <heymel@amazon.de>
2020-12-22 21:23:58 +0100

    Improved performance of day22 from 1.6s to 155ms
    After discussing and looking at https://github.com/shaeberling/euler/blob/master/kotlin/src/com/s13g/aoc/aoc2020/Day22.kt.

--- src/main/kotlin/com/scavi/brainsqueeze/adventofcode/Day22CrabCombat.kt
+++ src/main/kotlin/com/scavi/brainsqueeze/adventofcode/Day22CrabCombat.kt
@@ -21,18 +21,21 @@ class Day22CrabCombat {
             playersCards1: LinkedList<Int>,
             playersCards2: LinkedList<Int>,
             recursion: Boolean): Pair<Boolean, LinkedList<Int>> {
-        val seen = mutableSetOf<String>()
+        val seen = mutableSetOf<Int>()
         while (playersCards1.isNotEmpty() and playersCards2.isNotEmpty()) {
-            val cardsHash = "$playersCards1$playersCards2"
-            if (cardsHash in seen) {
+            if (playersCards1.hashCode() in seen || playersCards2.hashCode() in seen) {
                 return Pair(true, playersCards1)
             }
-            seen.add(cardsHash)
+            // Major improvement from 1,6 seconds to 155 ms after discussing / looking at
+            // https://github.com/shaeberling/euler/blob/master/kotlin/src/com/s13g/aoc/aoc2020/Day22.kt
+            seen.add(playersCards1.hashCode())
+            seen.add(playersCards2.hashCode())
             val card1 = playersCards1.pop()
             val card2 = playersCards2.pop()
             var player1Won = card1 > card2
             if (recursion && playersCards1.size >= card1 && playersCards2.size >= card2) {
-                player1Won = play(copy(playersCards1, card1), copy(playersCards2, card2), recursion).first
+                player1Won = play(LinkedList(playersCards1.subList(0, card1)),
+                        LinkedList(playersCards2.subList(0, card2)), recursion).first
             }
             if (player1Won) {
                 playersCards1.add(card1)
@@ -43,15 +46,6 @@ class Day22CrabCombat {
             }
         }
         return Pair(playersCards1.isNotEmpty(), if (playersCards1.isEmpty()) playersCards2 else playersCards1)
-    }
-
-    private fun copy(list: LinkedList<Int>, size: Int): LinkedList<Int> {
-        val newList = LinkedList<Int>()
-        var pos = 0
-        while (pos < size && pos < list.size) {
-            newList.add(list[pos++])
-        }
-        return newList
     }
 
     private fun getCards(input: List<String>): Pair<LinkedList<Int>, LinkedList<Int>> {
